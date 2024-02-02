43bd2f148c811a1d22f55f417e023d7c9ac8c1e8
Muhammad Rizky Rizaldi <myquinn489@gmail.com>
2024-02-02 10:55:33 +0800

    Formatting

--- src/fungsi/validasi.java
+++ src/fungsi/validasi.java
@@ -1478,49 +1478,42 @@ public final class validasi {
         }
     }
     
-    public String terbilang(double angka){
-        if(angka<12)
-        {
-          return nomina[(int)angka];
+    public String terbilang(double angka)
+    {
+        if (angka < 12) {
+            return nomina[(int) angka];
         }
-        
-        if(angka>=12 && angka <=19)
-        {
-            return nomina[(int)angka%10] +" belas ";
+
+        if (angka >= 12 && angka <= 19) {
+            return nomina[(int) angka % 10] + " belas ";
         }
-        
-        if(angka>=20 && angka <=99)
-        {
-            return nomina[(int)angka/10] +" puluh "+nomina[(int)angka%10];
+
+        if (angka >= 20 && angka <= 99) {
+            return nomina[(int) angka / 10] + " puluh " + nomina[(int) angka % 10];
         }
-        
-        if(angka>=100 && angka <=199)
-        {
-            return "seratus "+ terbilang(angka%100);
+
+        if (angka >= 100 && angka <= 199) {
+            return "seratus " + terbilang(angka % 100);
         }
-        
-        if(angka>=200 && angka <=999)
-        {
-            return nomina[(int)angka/100]+" ratus "+terbilang(angka%100);
+
+        if (angka >= 200 && angka <= 999) {
+            return nomina[(int) angka / 100] + " ratus " + terbilang(angka % 100);
         }
-        
-        if(angka>=1000 && angka <=1999)
-        {
-            return "seribu "+ terbilang(angka%1000);
+
+        if (angka >= 1_000 && angka <= 1_999) {
+            return "seribu " + terbilang(angka % 1_000);
         }
-        
-        if(angka >= 2000 && angka <=999999)
-        {
-            return terbilang((int)angka/1000)+" ribu "+ terbilang(angka%1000);
+
+        if (angka >= 2_000 && angka <= 999_999) {
+            return terbilang((int) angka / 1_000) + " ribu " + terbilang(angka % 1_000);
         }
-        
-        if(angka >= 1000000 && angka <=999999999)
-        {
-            return terbilang((int)angka/1000000)+" juta "+ terbilang(angka%1000000);
+
+        if (angka >= 1_000_000 && angka <= 999_999_999) {
+            return terbilang((int) angka / 1000000) + " juta " + terbilang(angka % 1000000);
         }
-        
+
         return "";
-    }  
+    }
     
     public int daysOld(String path) {
         file=new File(path);
