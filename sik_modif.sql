a76af1e1e7ab8edd917ab6ca1b0919aa00dedf3f
Muhammad Rizky Rizaldi <myquinn489@gmail.com>
2024-02-06 11:09:24 +0800

    Update sik_modif.sql

--- sik_modif.sql
+++ sik_modif.sql
@@ -20,6 +20,23 @@ ALTER TABLE `datasuplier` MODIFY COLUMN `no_telp` varchar(20) CHARACTER SET lati
 
 ALTER TABLE `datasuplier` MODIFY COLUMN `nama_bank` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL AFTER `no_telp`;
 
+CREATE TABLE `detail_pemberian_obat_selanjutnya`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `tgl_perawatan` date NULL DEFAULT NULL,
+  `jam` time NULL DEFAULT NULL,
+  `no_rawat` varchar(17) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `kode_brng` varchar(15) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `tgl_pemberian_selanjutnya` date NULL DEFAULT NULL,
+  `total_hari` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `detail_pemberian_obat_selanjutnya_reg_periksa_FK`(`no_rawat`) USING BTREE,
+  INDEX `detail_pemberian_obat_selanjutnya_databarang_FK`(`kode_brng`) USING BTREE,
+  INDEX `detail_pemberian_obat_selanjutnya_tgl_perawatan_IDX`(`tgl_perawatan`) USING BTREE,
+  INDEX `detail_pemberian_obat_selanjutnya_jam_IDX`(`jam`) USING BTREE,
+  CONSTRAINT `detail_pemberian_obat_selanjutnya_databarang_FK` FOREIGN KEY (`kode_brng`) REFERENCES `databarang` (`kode_brng`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `detail_pemberian_obat_selanjutnya_reg_periksa_FK` FOREIGN KEY (`no_rawat`) REFERENCES `reg_periksa` (`no_rawat`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
 ALTER TABLE `dokter` MODIFY COLUMN `almt_tgl` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL AFTER `agama`;
 
 ALTER TABLE `emergency_index` MODIFY COLUMN `nama_emergency` varchar(200) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL AFTER `kode_emergency`;
