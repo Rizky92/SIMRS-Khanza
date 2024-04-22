e74a7ae697fa0cdc3aa6730c7e0ad4aaa1c04b85
Muhammad Rizky Rizaldi <myquinn489@gmail.com>
2024-04-22 16:41:09 +0800

    Enable view billing pasien

--- webapps/berkasrawat/pages/billingklaim.php
+++ webapps/berkasrawat/pages/billingklaim.php
@@ -12,14 +12,14 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <link href="../css/default.css" rel="stylesheet" type="text/css" />
     </head>
-    <body bgcolor='#ffffff'>
+    <body bgcolor="#ffffff" style="max-width: 100vw; margin-right: 1.25rem">
     <?php
         $norawat = isset($_GET['iyem']) ? trim($_GET['iyem']) : null;
-        $norawat = json_decode(encrypt_decrypt($norawat, 'd'), true);
+        $norawat = encrypt_decrypt($norawat, 'd');
     ?>
-    <?php if (isset($norawat['no_rawat']) && isset($_SESSION['ses_vedika'])): ?>
+    <?php if (isset($norawat) && ($_SESSION['ses_admin_berkas_rawat'] ?? null) === 'billing'): ?>
         <?php
-            $no_rawat = validTeks4($norawat['no_rawat'], 20);
+            $no_rawat = validTeks4($norawat, 20);
             $_sql = "select billing.no,billing.nm_perawatan,billing.pemisah,if(billing.biaya=0,'',billing.biaya),if(billing.jumlah=0,'',billing.jumlah),if(billing.tambahan=0,'',billing.tambahan),if(billing.totalbiaya=0,'',billing.totalbiaya),billing.totalbiaya from billing where billing.no_rawat='$no_rawat' ";
             $hasil = bukaquery($_sql);
             $setting = mysqli_fetch_array(bukaquery("select setting.nama_instansi,setting.alamat_instansi,setting.kabupaten,setting.propinsi,setting.kontak,setting.email,setting.logo from setting"));
@@ -56,7 +56,7 @@
                     <?php $total += $inapdrpasien[7]; ?>
                     <?php if (empty($inapdrpasien[6]) && empty($inapdrpasien[0])): ?>
                         <tr class="isi12" padding="0">
-                            <td padding="0" width="18%"><font color="111111" size="1" face="Tahoma"><?= str_replace("   ","&nbsp;&nbsp;",$inapdrpasien[0]) ?></font></td>
+                            <td padding="0" width="18%"><font color="111111" size="1" face="Tahoma"><?= str_replace("   ", "&nbsp;&nbsp;", $inapdrpasien[0]) ?></font></td>
                             <td padding="0" width="40%" colspan="6" align="right"><font color="111111" size="1" face="Tahoma"><?= $inapdrpasien[1] ?></font></td>
                         </tr>
                     <?php else: ?>
