22a3bdf45c583d0e43a23b6163541f140128c8a5
Rizky92 <myquinn489@gmail.com>
2024-05-01 22:32:04 +0800

    Limit pencarian diagnosa pasien menggunakan icd10 eklaim

--- src/laporan/PanelDiagnosa.java
+++ src/laporan/PanelDiagnosa.java
@@ -6,11 +6,11 @@
 package laporan;
 
 import fungsi.WarnaTable;
+import fungsi.akses;
 import fungsi.batasInput;
 import fungsi.koneksiDB;
 import fungsi.sekuel;
 import fungsi.validasi;
-import fungsi.akses;
 import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.event.KeyEvent;
@@ -677,18 +677,14 @@ public class PanelDiagnosa extends widget.panelisi {
             for(i=0;i<jml;i++){
                 tabModeDiagnosa.addRow(new Object[] {pilih[i],kode[i],nama[i],ciripny[i],keterangan[i],kategori[i],cirium[i]});
             }       
-
-            pspenyakit=koneksi.prepareStatement("select penyakit.kd_penyakit,penyakit.nm_penyakit,penyakit.ciri_ciri,penyakit.keterangan, "+
-                    "kategori_penyakit.nm_kategori,kategori_penyakit.ciri_umum "+
-                    "from kategori_penyakit inner join penyakit "+
-                    "on penyakit.kd_ktg=kategori_penyakit.kd_ktg where  "+
-                    " penyakit.kd_penyakit like ? or "+
-                    " penyakit.nm_penyakit like ? or "+
-                    " penyakit.ciri_ciri like ? or "+
-                    " penyakit.keterangan like ? or "+
-                    " kategori_penyakit.nm_kategori like ? or "+
-                    " kategori_penyakit.ciri_umum like ? "+
-                    "order by penyakit.kd_penyakit  LIMIT 1000");
+            pspenyakit=koneksi.prepareStatement(
+                "select penyakit.kd_penyakit, penyakit.nm_penyakit, penyakit.ciri_ciri, penyakit.keterangan, " +
+                "kategori_penyakit.nm_kategori, kategori_penyakit.ciri_umum from kategori_penyakit " +
+                "inner join penyakit on penyakit.kd_ktg = kategori_penyakit.kd_ktg " +
+                "where exists(select * from eklaim_icd10 where eklaim_icd10.code = penyakit.kd_penyakit and eklaim_icd10.status = '1') and " +
+                "penyakit.kd_penyakit like ? or penyakit.nm_penyakit like ? or penyakit.ciri_ciri like ? or " +
+                "penyakit.keterangan like ? or kategori_penyakit.nm_kategori like ? or kategori_penyakit.ciri_umum like ? " +
+                "order by penyakit.kd_penyakit LIMIT 1000");
             try {
                 pspenyakit.setString(1,"%"+Diagnosa.getText().trim()+"%");
                 pspenyakit.setString(2,"%"+Diagnosa.getText().trim()+"%");
