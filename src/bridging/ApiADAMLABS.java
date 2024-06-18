e793200d5d64dabb8893692a47ff0ddb4a248549
Muhammad Rizky Rizaldi <myquinn489@gmail.com>
2024-06-18 08:13:35 +0800

    wip

--- src/bridging/ApiADAMLABS.java
+++ src/bridging/ApiADAMLABS.java
@@ -39,8 +39,10 @@ public class ApiADAMLABS
 {
     private final String APIURL = koneksiDB.ADAMLABSAPIURL(),
                          APIKEY = koneksiDB.ADAMLABSAPIKEY(),
-                         APIKODERS = koneksiDB.ADAMLABSAPIKODERS();
-    
+                         APIKODERS = koneksiDB.ADAMLABSAPIKODERS(),
+                         KECAMATANID = koneksiDB.ADAMLABSKECAMATANID(),
+                         KABUPATENID = koneksiDB.ADAMLABSKABUPATENID(),
+                         PROVINSIID = koneksiDB.ADAMLABSPROVINSIID();
     private final Connection koneksi = koneksiDB.condb();
     private final sekuel Sequel = new sekuel();
     private final SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
@@ -60,154 +62,95 @@ public class ApiADAMLABS
     
     public void registrasi(String kodeRegistrasi)
     {
-        System.out.println("No. Order : " + kodeRegistrasi);
         try {
-            ps = koneksi.prepareStatement(
-                "select " +
-                    "permintaan_lab.noorder, " +
-                    "permintaan_lab.diagnosa_klinis, " +
-                    "pasien.nm_pasien, " +
-                    "pasien.no_rkm_medis, " +
-                    "pasien.jk, " +
-                    "pasien.alamat, " +
-                    "pasien.no_tlp, " +
-                    "pasien.tgl_lahir, " +
-                    "pasien.no_ktp, " +
-                    "(select pemeriksaan_ralan.berat from pemeriksaan_ralan where (pemeriksaan_ralan.berat is not null and pemeriksaan_ralan.berat != '' and pemeriksaan_ralan.berat REGEXP '^[0-9,.]+$') " +
-                    "and pemeriksaan_ralan.no_rawat in (select r2.no_rawat from reg_periksa r2 where r2.no_rkm_medis = reg_periksa.no_rkm_medis) " +
-                    "order by pemeriksaan_ralan.no_rawat desc, pemeriksaan_ralan.tgl_perawatan desc, pemeriksaan_ralan.jam_rawat desc limit 1) as bb, " +
-                    "if (permintaan_lab.informasi_tambahan like '%cito%', 'Cito', 'Reguler') as jenis_registrasi, " +
-                    "pasien.kd_prop, " +
-                    "propinsi.nm_prop, " +
-                    "pasien.kd_kab, " +
-                    "kabupaten.nm_kab, " +
-                    "pasien.kd_kec, " +
-                    "kecamatan.nm_kec, " +
-                    "permintaan_lab.dokter_perujuk as kd_dokter_perujuk, " +
-                    "dokter_perujuk.nm_dokter as nm_dokter_perujuk, " +
-                    "reg_periksa.kd_poli, " +
-                    "poliklinik.nm_poli, " +
-                    "reg_periksa.kd_pj, " +
-                    "penjab.png_jawab " +
-                "from permintaan_lab " +
-                "join reg_periksa on permintaan_lab.no_rawat = reg_periksa.no_rawat " +
-                "join pasien on reg_periksa.no_rkm_medis = pasien.no_rkm_medis " +
-                "left join propinsi on pasien.kd_prop = propinsi.kd_prop " +
-                "left join kabupaten on pasien.kd_kab = kabupaten.kd_kab " +
-                "left join kecamatan on pasien.kd_kec = kecamatan.kd_kec " +
-                "join poliklinik on reg_periksa.kd_poli = poliklinik.kd_poli " +
-                "join penjab on reg_periksa.kd_pj = penjab.kd_pj " +
-                "join dokter dokter_perujuk on permintaan_lab.dokter_perujuk = dokter_perujuk.kd_dokter " +
-                "where permintaan_lab.noorder = ?"
-            );
-            try {
+            try (PreparedStatement ps = koneksi.prepareStatement(
+                "select permintaan_lab.noorder, permintaan_lab.diagnosa_klinis, pasien.nm_pasien, pasien.no_rkm_medis, pasien.jk, pasien.alamat, pasien.no_tlp, pasien.tgl_lahir, pasien.no_ktp, " +
+                "(select pemeriksaan_ralan.berat from pemeriksaan_ralan where (pemeriksaan_ralan.berat is not null and pemeriksaan_ralan.berat != '' and pemeriksaan_ralan.berat REGEXP '^[0-9,.]+$') " +
+                "and pemeriksaan_ralan.no_rawat in (select r2.no_rawat from reg_periksa r2 where r2.no_rkm_medis = reg_periksa.no_rkm_medis) order by pemeriksaan_ralan.no_rawat desc, pemeriksaan_ralan.tgl_perawatan desc, " +
+                "pemeriksaan_ralan.jam_rawat desc limit 1) as bb, if (permintaan_lab.informasi_tambahan like '%cito%', 'Cito', 'Reguler') as jenis_registrasi, permintaan_lab.dokter_perujuk as kd_dokter_perujuk, " +
+                "dokter_perujuk.nm_dokter as nm_dokter_perujuk, reg_periksa.kd_poli, poliklinik.nm_poli, reg_periksa.kd_pj, penjab.png_jawab from permintaan_lab join reg_periksa on permintaan_lab.no_rawat = reg_periksa.no_rawat " +
+                "join pasien on reg_periksa.no_rkm_medis = pasien.no_rkm_medis join poliklinik on reg_periksa.kd_poli = poliklinik.kd_poli join penjab on reg_periksa.kd_pj = penjab.kd_pj " +
+                "join dokter dokter_perujuk on permintaan_lab.dokter_perujuk = dokter_perujuk.kd_dokter where permintaan_lab.noorder = ?"
+            )) {
                 ps.setString(1, kodeRegistrasi);
-                rs = ps.executeQuery();
-                if (rs.next()) {
-                    jsonBuilder = "{" +
-                            "\"registrasi\": {" +
-                                "\"no_registrasi\": \"" + rs.getString("noorder") + "\"," +
-                                "\"diagnosa_awal\": \"-\"," +
-                                "\"keterangan_klinis\": \"" + rs.getString("diagnosa_klinis") + "\"," +
-                                "\"kode_rs\": \"" + APIKODERS + "\"" +
-                            "}," +
-                            "\"pasien\": {" +
-                                "\"nama\": \"" + rs.getString("nm_pasien") + "\"," +
-                                "\"no_rm\": \"" + rs.getString("no_rkm_medis") + "\"," +
-                                "\"jenis_kelamin\": \"" + rs.getString("jk") + "\"," +
-                                "\"alamat\": \"" + rs.getString("alamat") + "\"," +
-                                "\"no_telphone\": \"" + rs.getString("no_tlp") + "\"," +
-                                "\"tanggal_lahir\": \"" + df.format(rs.getDate("tgl_lahir")) + "\"," +
-                                "\"nik\": \"" + rs.getString("no_ktp") +"\"," +
-                                "\"ras\": \"-\"," +
-                                "\"berat_badan\": \"" + rs.getString("bb").toLowerCase().trim() + "kg\"," +
-                                "\"jenis_registrasi\" : \"" + rs.getString("jenis_registrasi") + "\"," +
-                                "\"m_provinsi_id\": \"" + rs.getString("nm_prop") + "\"," +
-                                "\"m_kabupaten_id\": \"" + rs.getString("nm_kab") + "\"," +
-                                "\"m_kecamatan_id\": \"" + rs.getString("nm_kec") + "\"" +
-                            "}," +
-                            "\"kode_dokter_pengirim\": \"" + rs.getString("kd_dokter_perujuk") + "\"," +
-                            "\"nama_dokter_pengirim\": \"" + rs.getString("nm_dokter_perujuk") + "\"," +
-                            "\"kode_unit_asal\": \"" + rs.getString("kd_poli") + "\"," +
-                            "\"nama_unit_asal\": \"" + rs.getString("nm_poli") + "\"," +
-                            "\"kode_penjamin\": \"" + rs.getString("kd_pj") + "\"," +
-                            "\"nama_penjamin\": \"" + rs.getString("png_jawab") + "\"," +
-                            "\"kode_icdt\": \"-\"," +
-                            "\"nama_icdt\": \"-\"," +
-                            "\"tindakan\": [";
-                    try {
-                        psdetail = koneksi.prepareStatement(
-                            "select permintaan_pemeriksaan_lab.kd_jenis_prw, jns_perawatan_lab.nm_perawatan " +
-                            "from permintaan_pemeriksaan_lab " +
-                            "join jns_perawatan_lab on permintaan_pemeriksaan_lab.kd_jenis_prw = jns_perawatan_lab.kd_jenis_prw " +
-                            "where permintaan_pemeriksaan_lab.noorder = ?"
-                        );
-                        try {
-                            psdetail.setString(1, kodeRegistrasi);
-                            rsdetail = psdetail.executeQuery();
-                            while (rsdetail.next()) {
-                                jsonBuilder = jsonBuilder + "{" +
-                                    "\"kode_tindakan\": \"" + rsdetail.getString("kd_jenis_prw") + "\"," +
-                                    "\"nama_tindakan\": \"" + rsdetail.getString("nm_perawatan") + "\"" +
-                                "},";
-                            }
-                            jsonBuilder = jsonBuilder.substring(0, jsonBuilder.length() - 1);
-                        } catch (Exception e) {
-                            System.out.println("Notif : " + e);
-                        } finally {
-                            if (rsdetail != null) {
-                                rsdetail.close();
-                            }
-                            if (psdetail != null) {
-                                psdetail.close();
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        jsonBuilder = "{"
+                            + "\"registrasi\": {"
+                            + "\"no_registrasi\": \"" + rs.getString("noorder") + "\","
+                            + "\"diagnosa_awal\": \"-\","
+                            + "\"keterangan_klinis\": \"" + rs.getString("diagnosa_klinis") + "\","
+                            + "\"kode_rs\": \"" + APIKODERS + "\""
+                            + "},"
+                            + "\"pasien\": {"
+                            + "\"nama\": \"" + rs.getString("nm_pasien") + "\","
+                            + "\"no_rm\": \"" + rs.getString("no_rkm_medis") + "\","
+                            + "\"jenis_kelamin\": \"" + rs.getString("jk") + "\","
+                            + "\"alamat\": \"" + rs.getString("alamat") + "\","
+                            + "\"no_telphone\": \"" + rs.getString("no_tlp") + "\","
+                            + "\"tanggal_lahir\": \"" + df.format(rs.getDate("tgl_lahir")) + "\","
+                            + "\"nik\": \"" + rs.getString("no_ktp") + "\","
+                            + "\"ras\": \"-\","
+                            + "\"berat_badan\": \"" + rs.getString("bb").toLowerCase().trim() + "kg\","
+                            + "\"jenis_registrasi\" : \"" + rs.getString("jenis_registrasi") + "\","
+                            + "\"m_provinsi_id\": \"" + PROVINSIID + "\","
+                            + "\"m_kabupaten_id\": \"" + KABUPATENID + "\","
+                            + "\"m_kecamatan_id\": \"" + KECAMATANID + "\""
+                            + "},"
+                            + "\"kode_dokter_pengirim\": \"" + rs.getString("kd_dokter_perujuk") + "\","
+                            + "\"nama_dokter_pengirim\": \"" + rs.getString("nm_dokter_perujuk") + "\","
+                            + "\"kode_unit_asal\": \"" + rs.getString("kd_poli") + "\","
+                            + "\"nama_unit_asal\": \"" + rs.getString("nm_poli") + "\","
+                            + "\"kode_penjamin\": \"" + rs.getString("kd_pj") + "\","
+                            + "\"nama_penjamin\": \"" + rs.getString("png_jawab") + "\","
+                            + "\"kode_icdt\": \"-\","
+                            + "\"nama_icdt\": \"-\","
+                            + "\"tindakan\": [";
+                        try (PreparedStatement ps2 = koneksi.prepareStatement(
+                            "select permintaan_pemeriksaan_lab.kd_jenis_prw, jns_perawatan_lab.nm_perawatan from permintaan_pemeriksaan_lab " +
+                            "join jns_perawatan_lab on permintaan_pemeriksaan_lab.kd_jenis_prw = jns_perawatan_lab.kd_jenis_prw where permintaan_pemeriksaan_lab.noorder = ?"
+                        )) {
+                            ps2.setString(1, kodeRegistrasi);
+                            try (ResultSet rs2 = ps2.executeQuery()) {
+                                while (rs2.next()) {
+                                    jsonBuilder = jsonBuilder + "{" +
+                                        "\"kode_tindakan\": \"" + rs2.getString("kd_jenis_prw") + "\"," +
+                                        "\"nama_tindakan\": \"" + rs2.getString("nm_perawatan") + "\"" +
+                                    "},";
+                                }
+                                jsonBuilder = jsonBuilder.substring(0, jsonBuilder.length() - 1);
                             }
                         }
-                    } catch (Exception e) {
-                        System.out.println("Notif : " + e);
+                        jsonBuilder = jsonBuilder + "]}";
                     }
-                    jsonBuilder = jsonBuilder + "]}";
-                }
-            } catch (Exception e) {
-                System.out.println("Notif : " + e);
-            } finally {
-                if (rs != null) {
-                    rs.close();
-                }
-                if (ps != null) {
-                    ps.close();
                 }
             }
-            try {
-                url = APIURL + "/bridging_sim_rs/registrasi";
-                System.out.println("URL : " + url);
-                System.out.println("JSON : " + jsonBuilder);
-                headers = new HttpHeaders();
-                headers.setContentType(MediaType.APPLICATION_JSON);
-                headers.add("x-api-key", APIKEY);
-                requestEntity = new HttpEntity(jsonBuilder, headers);
-                ResponseEntity<String> responseEntity = http().exchange(url, HttpMethod.POST, requestEntity, String.class);
-                System.out.println("Response : " + responseEntity.getBody());
-                System.out.println("Response : " + responseEntity.getStatusCode());
-                response = obj.readTree(responseEntity.getBody());
-
-                Sequel.menyimpanSmc(
-                    "adamlabs_request_response",
-                    "noorder, url, method, request, code, response, user_id",
-                    kodeRegistrasi, url, "POST", jsonBuilder, String.valueOf(responseEntity.getStatusCode()), responseEntity.getBody(), akses.getkode()
-                );
-                if (response.path("code").asText().equals("200")) {
-                    Sequel.menyimpanSmc("adamlabs_orderlab", null, kodeRegistrasi, response.path("registrasi").path("no_lab").asText());
-                }
-            } catch (HttpClientErrorException e) {
-                System.out.println(e.getResponseBodyAsString());
-            } catch (Exception e) {
-                System.out.println("Notif : " + e);
-                if (e.getMessage().contains("UnknownHostException")) {
-                    System.out.println("Sambungan ke server ADAMLABS terputus!");
-                }
+            url = APIURL + "/bridging_sim_rs/registrasi";
+            System.out.println("URL : " + url);
+            System.out.println("JSON : " + jsonBuilder);
+            headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            headers.add("x-api-key", APIKEY);
+            requestEntity = new HttpEntity(jsonBuilder, headers);
+            ResponseEntity<String> responseEntity = http().exchange(url, HttpMethod.POST, requestEntity, String.class);
+            System.out.println("Response : " + responseEntity.getBody());
+            System.out.println("Response : " + responseEntity.getStatusCode());
+            response = obj.readTree(responseEntity.getBody());
+            Sequel.menyimpanSmc(
+                "adamlabs_request_response",
+                "noorder, url, method, request, code, response, user_id",
+                kodeRegistrasi, url, "POST", jsonBuilder, String.valueOf(responseEntity.getStatusCode()), responseEntity.getBody(), akses.getkode()
+            );
+            if (response.path("code").asText().equals("200")) {
+                Sequel.menyimpanSmc("adamlabs_orderlab", null, kodeRegistrasi, response.path("registrasi").path("no_lab").asText());
             }
+        } catch (HttpClientErrorException e) {
+            System.out.println(e.getResponseBodyAsString());
         } catch (Exception e) {
             System.out.println("Notif : " + e);
+            if (e.getMessage().contains("UnknownHostException")) {
+                System.out.println("Sambungan ke server ADAMLABS terputus!");
+            }
         }
     }
     
