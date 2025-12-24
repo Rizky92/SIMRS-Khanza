55cada847b0d025458f4bebfce535c3df597b1ec
Rizky92 <myquinn489@gmail.com>
2025-12-24 21:30:13 +0800

    tampjurnal
    Signed-off-by: Rizky92 <myquinn489@gmail.com>

--- src/bridging/ApotekBPJSResepObat.java
+++ src/bridging/ApotekBPJSResepObat.java
@@ -1557,27 +1557,17 @@ private void BtnSimpanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIR
                     }
                     
                     if(sukses==true){
-                        Sequel.queryu("delete from tampjurnal");    
+                        Sequel.deleteTampJurnal();
                         if(ttljual>0){
-                            if(Sequel.menyimpantf2("tampjurnal","'"+Suspen_Piutang_Obat_Ralan+"','Suspen Piutang Obat Ralan','"+ttljual+"','0'","Rekening")==false){
-                                sukses=false;
-                            }    
-                            if(Sequel.menyimpantf2("tampjurnal","'"+Obat_Ralan+"','Pendapatan Obat Rawat Jalan','0','"+ttljual+"'","Rekening")==false){
-                                sukses=false;
-                            }                              
+                            if (sukses) sukses = Sequel.insertTampJurnal(Suspen_Piutang_Obat_Ralan, "Suspen Piutang Obat Ralan", ttljual, 0);
+                            if (sukses) sukses = Sequel.insertTampJurnal(Obat_Ralan, "Pendapatan Obat Rawat Jalan", 0, ttljual);
                         }
                         if(ttlhpp>0){
-                            if(Sequel.menyimpantf2("tampjurnal","'"+HPP_Obat_Rawat_Jalan+"','HPP Persediaan Obat Rawat Jalan','"+ttlhpp+"','0'","Rekening")==false){
-                                sukses=false;
-                            }    
-                            if(Sequel.menyimpantf2("tampjurnal","'"+Persediaan_Obat_Rawat_Jalan+"','Persediaan Obat Rawat Jalan','0','"+ttlhpp+"'","Rekening")==false){
-                                sukses=false;
-                            }                              
+                            if (sukses) sukses = Sequel.insertTampJurnal(HPP_Obat_Rawat_Jalan, "HPP Persediaan Obat Rawat Jalan", ttlhpp, 0);
+                            if (sukses) sukses = Sequel.insertTampJurnal(Persediaan_Obat_Rawat_Jalan, "Persediaan Obat Rawat Jalan", 0, ttlhpp);
                         }
-                        if(sukses==true){
-                            if((ttljual>0)||(ttlhpp>0)){
-                                sukses=jur.simpanJurnal(TNoRw.getText(),"U","PEMBERIAN OBAT RAWAT JALAN PASIEN "+TNoRM.getText()+" "+TPasien.getText()+", DIPOSTING OLEH "+akses.getkode());     
-                            }
+                        if (ttljual > 0 || ttlhpp > 0) {
+                            if (sukses) sukses = jur.simpanJurnal(TNoRw.getText(), "U", "PEMBERIAN OBAT RAWAT JALAN PASIEN " + TNoRM.getText() + " " + TPasien.getText() + ", DIPOSTING OLEH " + akses.getkode());
                         }
                     }
                     
