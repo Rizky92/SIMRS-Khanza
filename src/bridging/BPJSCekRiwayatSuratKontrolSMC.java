81eef56195b5d5a889240f21247f49811d3c6c36
Muhammad Rizky Rizaldi <myquinn489@gmail.com>
2026-02-06 09:00:24 +0800

    wipwip
    Signed-off-by: Muhammad Rizky Rizaldi <myquinn489@gmail.com>

--- src/bridging/BPJSCekRiwayatSuratKontrolSMC.java
+++ src/bridging/BPJSCekRiwayatSuratKontrolSMC.java
@@ -184,7 +184,7 @@ public final class BPJSCekRiwayatSuratKontrolSMC extends javax.swing.JDialog {
             }
         });
 
-        internalFrame1.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(240, 245, 235)), "::[ Pencarian Riwayat Rujukan VClaim ]::", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Tahoma", 0, 11), new java.awt.Color(50, 50, 50))); // NOI18N
+        internalFrame1.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(240, 245, 235)), "::[ Pencarian Riwayat Surat Kontrol VClaim ]::", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Tahoma", 0, 11), new java.awt.Color(50, 50, 50))); // NOI18N
         internalFrame1.setName("internalFrame1"); // NOI18N
         internalFrame1.setLayout(new java.awt.BorderLayout(1, 1));
 
@@ -276,6 +276,7 @@ public final class BPJSCekRiwayatSuratKontrolSMC extends javax.swing.JDialog {
         panelGlass6.add(jLabel17);
 
         BtnKeluar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picture/exit.png"))); // NOI18N
+        BtnKeluar.setMnemonic('K');
         BtnKeluar.setText("Keluar");
         BtnKeluar.setToolTipText("Alt+K");
         BtnKeluar.setName("BtnKeluar"); // NOI18N
