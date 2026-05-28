fa66b7b3e7491d775599e7ec234786f72ecb8947
Muhammad Rizky Rizaldi <myquinn489@gmail.com>
2026-05-28 08:28:47 +0800

    wip
    Signed-off-by: Muhammad Rizky Rizaldi <myquinn489@gmail.com>

--- src/rekammedis/RMRiwayatPerawatan.java
+++ src/rekammedis/RMRiwayatPerawatan.java
@@ -5881,6 +5881,7 @@ public final class RMRiwayatPerawatan extends javax.swing.JDialog {
                                                     do{
                                                         switch (rs3.getString("keterangan").toLowerCase()) {
                                                             case "l":
+                                                            case "cl":
                                                             case "kl":
                                                                 htmlContent.append(
                                                                     "<tr>").append(
@@ -5894,6 +5895,7 @@ public final class RMRiwayatPerawatan extends javax.swing.JDialog {
                                                                     "</tr>");
                                                                 break;
                                                             case "h":
+                                                            case "ch":
                                                             case "kh":
                                                             case "tn":
                                                                 htmlContent.append(
@@ -7773,6 +7775,7 @@ public final class RMRiwayatPerawatan extends javax.swing.JDialog {
                                                 while (rs3.next()) {
                                                     switch (rs3.getString("keterangan").toLowerCase()) {
                                                         case "l":
+                                                        case "cl":
                                                         case "kl":
                                                             htmlContent.append(
                                                                 "<tr>"
@@ -7786,6 +7789,7 @@ public final class RMRiwayatPerawatan extends javax.swing.JDialog {
                                                                 + "</tr>");
                                                             break;
                                                         case "h":
+                                                        case "ch":
                                                         case "kh":
                                                         case "tn":
                                                             htmlContent.append(
